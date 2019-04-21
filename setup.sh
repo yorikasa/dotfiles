b3f625928010a9a326c2b6811408368bff8882cd
Yuki Orikasa <yuki.wter@gmail.com>
2019-04-21 15:42:20 +0900

    Add .gitignore_global

--- setup.sh
+++ setup.sh
@@ -3,4 +3,5 @@ ln -s $(pwd)/.bashrc ~/.bashrc
 ln -s $(pwd)/.bash_profile ~/.bash_profile
 ln -s $(pwd)/.inputrc ~/.inputrc
 ln -s $(pwd)/.gitconfig ~/.gitconfig
+ln -s $(pwd)/.gitignore_global ~/.gitignore_global
 ln -s $(pwd)/git-completion.bash ~/.git-completion.bash
